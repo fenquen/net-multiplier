6dc7fabc7a1a9524cb7725f306c9a32175b1c46b
fenquen <fenquen@163.com>
2019-09-11 16:40:56 +0800

    modify the aotomic counter let it more reliable

--- utils/atomic.go
+++ utils/atomic.go
@@ -20,19 +20,42 @@ func GetAndAddInt32WithDelta(target *int32, delta int32) int32 {
 func GetCyclic(target *int32, delta int32, max int32, base int32) int32 {
 	result := GetAndAddInt32WithDelta(target, delta)
 
+	for {
+		if result >= base {
+			break
+		}
+
+		result = *target
+
+		if result >= base {
+			if atomic.CompareAndSwapInt32(target, result, result+delta) {
+				break
+			}
+			continue
+		}
+
+		if atomic.CompareAndSwapInt32(target, result, base+delta) {
+			result = base
+			break
+		}
+	}
+
 	if max > result {
 		return result
 	}
 
 	for {
-		nowVal := *target
-		if max > nowVal {
-			return nowVal
+		result = *target
+		if max > result {
+			if !atomic.CompareAndSwapInt32(target, result, result+delta) {
+				continue
+			}
+			return result
 		}
 
-		swapSuccess := atomic.CompareAndSwapInt32(target, nowVal, base+delta)
-		if swapSuccess {
+		if atomic.CompareAndSwapInt32(target, result, base+delta) {
 			return base
 		}
+
 	}
 }
