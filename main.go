87b7c5357f831f3dd9c4b42cbafee52350f77400
fenquen <fenquen@163.com>
2019-09-11 11:10:45 +0800

    cmd param durability improved

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"os"
+	"strings"
 	"tcp-multiplier/config"
 )
 
 func main() {
 	fmt.Println(config.LocalTcpSvrAddr)
-	fmt.Println(config.DestTcpSvrAddr)
+	fmt.Println(config.DestTcpSvrAddrs)
+
+	if "" == strings.Trim(config.DestTcpSvrAddrs, " ") {
+		log.Println("you actually did not specify a valid \"DestTcpSvrAddrs\",it is virtually empty")
+		flag.Usage()
+		os.Exit(0)
+	}
 }
