36c1183e532a302b0161f54fe5badf7f7c94463c
fenquen <fenquen@163.com>
2019-09-11 13:30:05 +0800

    add management machanism for senders per srcTcpConn

--- server/listener.go
+++ server/listener.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"io"
+	"log"
 	"net"
 	"strings"
 	"tcp-multiplier/client"
@@ -12,6 +14,8 @@ func ListenAndServe() {
 
 	for {
 		localTcpSvrAddr, err := net.ResolveTCPAddr(config.TCP_TYPE, config.LocalTcpSvrAddr)
+
+		// fail to bind local address
 		if nil != err {
 			panic(err)
 		}
@@ -22,26 +26,52 @@ func ListenAndServe() {
 
 		// goroutine for single srcTcpConn
 		go func() {
-			srcDataChanSlice := make([]chan []byte, len(destTcpSvrAddrStrSlice), len(destTcpSvrAddrStrSlice))
+			destNum := len(destTcpSvrAddrStrSlice)
+
+			srcDataChanSlice := make([]chan []byte, destNum, destNum)
+			senderSlice := make([]*client.Sender, destNum, destNum)
 
 			for a, destTcpSvrAddrStr := range destTcpSvrAddrStrSlice {
 				srcDataChan := make(chan []byte, 100)
 				srcDataChanSlice[a] = srcDataChan
 
 				sender, err := client.NewSender(srcDataChan, destTcpSvrAddrStr)
+
+				// fail to build sender,due to net err
 				if nil != err {
+					srcDataChanSlice[a] = nil
 					continue
 				}
+
+				senderSlice[a] = sender
+
 				sender.Start()
 			}
 
 			for {
 				tempByteSlice := make([]byte, 1024, 1024)
-				readCount, _ := srcTcpConn.Read(tempByteSlice)
+
+				readCount, err := srcTcpConn.Read(tempByteSlice)
+
+				// meanings srcTcpConn is closed by client
+				if err == io.EOF {
+					log.Println("srcTcpConn.Read EOF,srcTcpConn is closed by client")
+
+					// interrupt all sender serving this srcTcpConn
+					for _, sender := range senderSlice {
+						if nil != sender {
+							sender.Interrupt()
+						}
+					}
+				}
+
 				tempByteSlice = tempByteSlice[0:readCount]
 
 				// need to dispatch data to each sender's data channel
 				for _, srcDataChan := range srcDataChanSlice {
+					if nil == srcDataChan {
+						continue
+					}
 					srcDataChan <- tempByteSlice
 				}
 			}
