c1ca84a0c796f27c2d4166c6cc5b2639220b5d25
fenquen <fenquen@163.com>
2019-09-11 11:21:52 +0800

    add support for read LocalTcpClientHost form command line

--- config/config.go
+++ config/config.go
@@ -3,10 +3,20 @@ package config
 import "flag"
 
 const TCP_TYPE = "tcp"
+const DELIMITER = ","
 
 var (
-	LocalTcpSvrAddr = *(flag.String("localTcpSvrAddr", "0.0.0.0:9070", "the address where the server listens"))
-	DestTcpSvrAddr  = *(flag.String("destTcpSvrAddr", "", "the destinations that the data is relayed to,it is a comma-delimited string"))
+	LocalTcpSvrAddr = *(flag.String("localTcpSvrAddr",
+		"0.0.0.0:9070",
+		"the address where the server listens"))
+
+	DestTcpSvrAddrs = *(flag.String("destTcpSvrAddr",
+		"",
+		"the destinations that the data is relayed to,it is a comma-delimited string,e.g. 192.168.1.6:9060,192.168.1.60:9060"))
+
+	LocalTcpClientHost = *(flag.String("localTcpClientHost",
+		"0.0.0.0",
+		"designate the host to which the sender is bind to"))
 )
 
 func init() {
